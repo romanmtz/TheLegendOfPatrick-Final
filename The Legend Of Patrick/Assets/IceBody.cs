8ebe1d2bcc2078bc7287ab760c61cc0684807ab2
romanmtz <113059087+romanmtz@users.noreply.github.com>
2023-08-01 14:18:38 -0500

    Started On Last Level; Added Functionality to Ice Bodies

--- The Legend Of Patrick/Assets/IceBody.cs
+++ The Legend Of Patrick/Assets/IceBody.cs
@@ -7,12 +7,20 @@ public class IceBody : MonoBehaviour
     public bool isIced = false;
     public float freezeTime = 1;
     public float timer = 0;
+
     Rigidbody2D rb;
+    SpriteRenderer sr;
+
+    Color originalColor;
+
 
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+
+        originalColor = new Color(sr.material.color.r,sr.material.color.g,sr.material.color.b,sr.material.color.a);
     }
     public void IceObject()
     {
@@ -25,11 +33,16 @@ public class IceBody : MonoBehaviour
         if (timer > 0)
         {
             timer -= Time.fixedDeltaTime;
+            
             isIced = true;
+            sr.material.SetColor("_Color", new Color(0,1.57f,1.95f,0.5f));
+
 
         }
         else
         {
+
+            sr.material.SetColor("_Color", originalColor);
             isIced = false;
         }
 
